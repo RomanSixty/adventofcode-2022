ba321467b2c0c82e06917c1b034a8eff13e111c3
Alexander Schulze <github@lxhome.de>
2022-12-21 19:48:49 +0100

    Day 2

---
+++ 02/solve.php
@@ -0,0 +1,45 @@
+<?php
+
+$input = file_get_contents ( __DIR__ . '/input.txt' );
+
+$rounds = explode ( "\n", $input );
+
+// there's 9 possible games... let's just list their outcomes
+$games = [
+    'A X' => 4,
+    'A Y' => 8,
+    'A Z' => 3,
+    'B X' => 1,
+    'B Y' => 5,
+    'B Z' => 9,
+    'C X' => 7,
+    'C Y' => 2,
+    'C Z' => 6
+];
+
+$points = 0;
+
+foreach ( $rounds as $round )
+    $points += $games [ $round ];
+
+echo "First part: $points\n";
+
+// alright... 9 different outcomes, still easy to precalculate
+$games2 = [
+    'A X' => 3,
+    'A Y' => 4,
+    'A Z' => 8,
+    'B X' => 1,
+    'B Y' => 5,
+    'B Z' => 9,
+    'C X' => 2,
+    'C Y' => 6,
+    'C Z' => 7
+];
+
+$points2 = 0;
+
+foreach ( $rounds as $round )
+    $points2 += $games2 [ $round ];
+
+echo "Second part: $points2\n";
