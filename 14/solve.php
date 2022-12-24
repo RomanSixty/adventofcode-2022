e3cc2895d0551389f7d864806e01c5f71390544a
Alexander Schulze <github@lxhome.de>
2022-12-25 00:24:23 +0100

    Day 14

---
+++ 14/solve.php
@@ -0,0 +1,131 @@
+<?php
+
+//include __DIR__ . '/visualize.php';
+
+$input = file ( __DIR__ . '/input.txt', FILE_IGNORE_NEW_LINES );
+
+$viewport = [
+    'top'    => 0,
+    'left'   => 500,
+    'right'  => 500,
+    'bottom' => 0
+];
+
+$map = [];
+
+foreach ( $input as $line )
+{
+    $points = [];
+
+    $coords = explode ( ' -> ', $line );
+
+    $point_a = array_pop ( $coords );
+
+    while ( $point_b = array_pop ( $coords ) )
+    {
+        draw_line ( $point_a, $point_b );
+
+        $point_a = $point_b;
+    }
+}
+
+function draw_line ( $a, $b )
+{
+    global $map, $viewport;
+
+    $a = explode ( ',', $a );
+    $b = explode ( ',', $b );
+
+    $viewport [ 'left' ] = min ( $viewport [ 'left' ], $a [ 0 ] );
+    $viewport [ 'left' ] = min ( $viewport [ 'left' ], $b [ 0 ] );
+
+    $viewport [ 'right' ] = max ( $viewport [ 'right' ], $a [ 0 ] );
+    $viewport [ 'right' ] = max ( $viewport [ 'right' ], $b [ 0 ] );
+
+    $viewport [ 'bottom' ] = max ( $viewport [ 'bottom' ], $a [ 1 ] );
+    $viewport [ 'bottom' ] = max ( $viewport [ 'bottom' ], $b [ 1 ] );
+
+    if ( $a [ 0 ] == $b [ 0 ] )
+    {
+        if ( $a [ 1 ] < $b [ 1 ] )
+            for ( $y = $a [ 1 ]; $y <= $b [ 1 ]; $y++ )
+                $map [ $y ][ $a [ 0 ]] = 'X';
+        else
+            for ( $y = $b [ 1 ]; $y <= $a [ 1 ]; $y++ )
+                $map [ $y ][ $a [ 0 ]] = 'X';
+    }
+    else
+    {
+        if ( $a [ 0 ] < $b [ 0 ] )
+            for ( $x = $a [ 0 ]; $x <= $b [ 0 ]; $x++ )
+                $map [ $a [ 1 ]][ $x ] = 'X';
+        else
+            for ( $x = $b [ 0 ]; $x <= $a [ 0 ]; $x++ )
+                $map [ $a [ 1 ]][ $x ] = 'X';
+    }
+}
+
+function trickle ( &$map, $viewport, $sand )
+{
+    while ( true )
+    {
+        // out of bounds
+        if ( $sand [ 1 ] > $viewport [ 'bottom' ] )
+            return false;
+
+        // down
+        elseif ( empty ( $map [ $sand [ 1 ] + 1 ][ $sand [ 0 ]] ) )
+        {
+            $sand [ 1 ]++;
+        }
+
+        // left
+        elseif ( empty ( $map [ $sand [ 1 ] + 1 ][ $sand [ 0 ] - 1 ] ) )
+        {
+            $sand [ 0 ]--;
+            $sand [ 1 ]++;
+        }
+
+        // right
+        elseif ( empty ( $map [ $sand [ 1 ] + 1 ][ $sand [ 0 ] + 1 ] ) )
+        {
+            $sand [ 0 ]++;
+            $sand [ 1 ]++;
+        }
+
+        // stop
+        else
+        {
+            $map [ $sand [ 1 ]][ $sand [ 0 ]] = 'o';
+
+            return !( $sand [ 0 ] == 500 && $sand [ 1 ] == 0 );
+        }
+    }
+}
+
+function pour ( $map, $viewport, $part = 1 )
+{
+    $counter = 0;
+
+    while ( trickle ( $map, $viewport, [ 500, 0 ] ) )
+        $counter++;
+
+    //draw_map ( $map, $viewport );
+    //createSVG ( $map, $viewport, $part );
+
+    return $counter;
+}
+
+echo 'First part: ' . pour ( $map, $viewport ) . "\n";
+
+// let's build the floor making some pretentious assumptions about the maximum expansion
+// if there were no obstacles a perfect pile would be double its height
+
+$viewport [ 'bottom' ] += 2;
+
+draw_line (
+    500 - $viewport [ 'bottom' ] . ',' . $viewport [ 'bottom' ],
+    500 + $viewport [ 'bottom' ] . ',' . $viewport [ 'bottom' ]
+);
+
+echo 'Second part: ' . pour ( $map, $viewport, 2 ) + 1 . "\n";
---
+++ 14/visualize.php
@@ -0,0 +1,31 @@
+<?php
+
+function draw_map ( $map, $viewport )
+{
+    for ( $y = $viewport [ 'top' ]; $y <= $viewport [ 'bottom' ]; $y++ )
+    {
+        for ( $x = $viewport [ 'left' ]; $x <= $viewport [ 'right' ]; $x++ )
+            echo $map [ $y ][ $x ] ?? '.';
+
+        echo "\n";
+    }
+}
+
+function createSVG ( $map, $viewport, $part )
+{
+    $svg[] = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="' . $viewport [ 'left' ] . ' 0 ' . $viewport [ 'right' ] - $viewport [ 'left' ] . ' ' . $viewport [ 'bottom' ] . '">';
+
+    // walls
+    foreach ( $map as $y => $point )
+        foreach ( $point as $x => $content )
+            {
+                if ( $content == 'X' )
+                    $svg[] = '<rect x="' . $x . '" y="' . $y . '" width="1" height="1" fill="black"/>';
+                else
+                    $svg[] = '<rect x="' . $x . '" y="' . $y . '" width="1" height="1" fill="orange"/>';
+            }
+
+    $svg[] = '</svg>';
+
+    file_put_contents ( __DIR__ . '/output_' . $part . '.svg', implode ( "\n", $svg ) );
+}
