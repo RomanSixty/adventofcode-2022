2814f08a1a547210929f944cac3f8835eb61c2f9
Alexander Schulze <github@lxhome.de>
2022-12-24 17:01:11 +0100

    Day 10

---
+++ 10/solve.php
@@ -0,0 +1,45 @@
+<?php
+
+$input = file ( __DIR__ . '/input.txt', FILE_IGNORE_NEW_LINES );
+
+$X = 1;
+$cycles = [ $X ];
+
+foreach ( $input as $instruction )
+{
+    if ( $instruction == 'noop' )
+        $cycles[] = $X;
+    else
+    {
+        $cycles[] = $X;
+
+        list ( $operation, $value ) = explode ( ' ', $instruction );
+
+        $X += $value;
+
+        $cycles[] = $X;
+    }
+}
+
+$signal_strength_sum = 0;
+
+foreach ( [20, 60, 100, 140, 180, 220] as $cycle )
+    $signal_strength_sum += $cycle * $cycles [ $cycle - 1 ];
+
+echo "First part: $signal_strength_sum\n";
+
+echo "\nSecond part:\n";
+
+for ( $pixel = 0; $pixel < 240; $pixel++ )
+{
+    $pixel_in_line = $pixel % 40;
+
+    if (    $cycles [ $pixel ] >= $pixel_in_line - 1
+         && $cycles [ $pixel ] <= $pixel_in_line + 1 )
+        echo '█';
+    else
+        echo '∙';
+
+    if ( ($pixel + 1) % 40 == 0 )
+        echo "\n";
+}
