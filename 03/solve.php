8e44373d9574dd95d9ebeb5ebe8d3d7ebc28e7bf
Alexander Schulze <github@lxhome.de>
2022-12-28 18:08:44 +0100

    Day 3 simplification

--- 03/solve.php
+++ 03/solve.php
@@ -8,10 +8,10 @@ foreach ( $rucksacks as &$rucksack )
 {
     $compartment_size = strlen ( $rucksack ) / 2;
 
-    $compartments = explode ( '+', chunk_split ( $rucksack, $compartment_size, '+' ) );
+    $compartments = str_split ( $rucksack, $compartment_size );
 
-    // items are only counted once, even if they appear multiple times in the same compartment
-    // hence we unify...
+    // items are only counted once, even if they appear multiple times in
+    // the same compartment hence we unify...
 
     $content = str_split ( $compartments [ 0 ] );
 
@@ -20,13 +20,6 @@ foreach ( $rucksacks as &$rucksack )
     foreach ( $content as $item )
         if ( strpos ( $compartments [ 1 ], $item ) !== false )
             $prio_sum += getPriority ( $item );
-}
-
-function getPriority ( $item )
-{
-    $val = ord ( $item );
-
-    return ( $val > 90 ) ? $val - 96 : $val - 38;
 }
 
 echo "First part: $prio_sum\n";
@@ -42,20 +35,25 @@ while ( count ( $rucksacks ) )
     ];
 
     // sort by rucksack size to compare the fewest items
-
     usort ( $group, function ( $a, $b ){
         return strlen ( $a ) > strlen ( $b );
     });
 
     for ( $i = 0; $i < strlen ( $group [ 0 ] ); $i++ )
-    {
         if (    strpos ( $group [ 1 ], $group [ 0 ][ $i ] ) !== false
              && strpos ( $group [ 2 ], $group [ 0 ][ $i ] ) !== false )
         {
             $badge_sum += getPriority ( $group [ 0 ][ $i ] );
             break;
         }
-    }
 }
 
 echo "Second part: $badge_sum\n";
+
+function getPriority ( $item )
+{
+    $val = ord ( $item );
+
+    // lowercase: 97-122, uppercase: 65 - 90
+    return ( $val > 90 ) ? $val - 96 : $val - 38;
+}
